4406b36e07a9be3ae2f1ac48cc994554739d6e9d
Suji Cho <89967107+csj1430@users.noreply.github.com>
2023-01-16 21:50:52 +0900

    Add files via upload

---
+++ 0116/B10828/src/Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int[] stack = new int[10000]; //스택 생성
+    public static int topIndex = -1; //스택 제일 위에 있는 원소를 가르키는 변수 생성
+
+    public static void push(int x) {
+        topIndex++; //하나 쌓이게 변수를 늘려줌
+        stack[topIndex] = x; //늘려진 스택에 원소 추가
+    }
+
+    public static int pop() {
+        if (topIndex < 0) {
+            return -1; //스택에 아무것도 없으면 -1 반환
+        }
+        else {
+            int temp = stack[topIndex]; //제일 위의 원소를 temp에 저장
+            topIndex--; //스택의 크기를 1 감소시켜줌
+            return temp; //pop시킨 원소 반환
+        }
+    }
+
+    public static int size() {
+        if (topIndex < 0) {
+            return -1; //스택에 아무것도 없으면 -1 반환
+        }
+        else {
+            return topIndex + 1; //topIndex는 인덱스 값을 반환하므로 1을 더해 스택에 저장된 원소 개수 반환
+        }
+    }
+
+    public static int empty() {
+        if (topIndex < 0) {
+            return 1; //스택이 비어있으면 1 반환
+        }
+        else {
+            return 0; //스택에 원소가 있으면 0 반환
+        }
+    }
+
+    public static int top() {
+        if (topIndex < 0) {
+            return -1; //스택이 비어있으면 -1 반환
+        }
+        else {
+            return stack[topIndex]; //제일 위의 정수 반환
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); //명령어 개수 입력 받기
+
+        for (int i = 0; i < n; i++) {
+            String str = sc.next();
+
+            switch (str) {
+                case "push" -> push(sc.nextInt());
+                case "pop" -> pop();
+                case "size" -> size();
+                case "empty" -> empty();
+                case "top" -> top();
+            }
+        }
+    }
+}
---
+++ 0116/B9012/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static char[] stack = new char[50]; //스택 생성
+    public static int topIndex = -1; //제일 위 원소의 인덱스 반환 변수
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); //괄호 문자열을 입력받을 횟수
+
+        for (int i = 0; i < n; i++) {
+            String str = sc.next(); //괄호 문자열 입력받기
+            boolean check = true;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '(') { //여는 괄호일 때
+                    topIndex++;
+                    stack[topIndex] = str.charAt(j); //스택에 저장
+                    break;
+                }
+                else if (str.charAt(j) == ')') { //닫는 괄호일 때
+                    if (topIndex == -1) { //스택에 아무것도 없을 때
+                        check = false; //false로 바꿔 문자열에 닫는 괄호가 많다는 것을 표현
+                    }
+                    else { //스택에 여는 괄호가 저장 돼 있을 때
+                        stack[topIndex] = 0; //pop
+                        topIndex--;
+                    }
+                }
+            }
+
+            if (topIndex == -1 && check) {
+                System.out.println("YES"); //스택에 원소가 없을 때 yes
+            }
+            else {
+                System.out.println("NO"); //스택에 원소가 있으면 no
+            }
+        }
+    }
+}
